41f457020fc527ea5c1ea6431c81f1959ac89fa0
duvalier tsagmo <edtsagmo01@monccnb.ca>
2022-12-13 09:50:06 -0400

    first commit

---
+++ include/Secrets.h
@@ -0,0 +1,57 @@
+/*
+SSID du WIFI ainsi que le mot de passe
+Modifier les paramètre WIFI pour le réseau auquels votre objets doit se brancher
+*/
+
+//#define WIFI_SECRET_SSID "IDO2"
+//#define WIFI_SECRET_PASS "99Bidules!"
+
+//#define WIFI_SECRET_SSID "UNIFI_IDO2"
+//#define WIFI_SECRET_PASS "99Bidules!"
+
+#define WIFI_SECRET_SSID "IDO"
+#define WIFI_SECRET_PASS "99Bidules!"
+
+
+//#define WIFI_SECRET_SSID "RogersCmr"
+//#define WIFI_SECRET_PASS "CMR062021"
+
+
+//#define WIFI_SECRET_SSID "HerveStella"
+//#define WIFI_SECRET_PASS "Maman123"
+
+//#define WIFI_SECRET_SSID "FibreOP532"
+//#define WIFI_SECRET_PASS "9PXPE66PM6XM55M8"
+
+
+/*
+Information nécessaire pour le branchement a un Broker MQTT
+Dans le cadre du cours AIIA1013, ne modifié pas cet information
+*/
+//#define SECRET_MQTT_SERVER_IP "192.168.2.99"
+//#define SECRET_MQTT_SERVER_IP "10.0.0.100"
+//#define SECRET_MQTT_SERVER_PORT 1883
+
+//#define SECRET_MQTT_SERVER_IP "192.168.2.50"
+//#define SECRET_MQTT_SERVER_PORT 1883
+
+//#define SECRET_MQTT_SERVER_IP "192.168.2.50"
+//#define SECRET_MQTT_SERVER_PORT 1883
+
+
+#define SECRET_MQTT_SERVER_IP "198.164.130.74"
+#define SECRET_MQTT_SERVER_PORT 1883
+
+//#define SECRET_MQTT_SERVER_IP "192.168.1.21"
+//#define SECRET_MQTT_SERVER_PORT 1883
+
+//#define SECRET_MQTT_SERVER_IP "10.124.199.21"
+//#define SECRET_MQTT_SERVER_PORT 1883
+
+/*
+Détails sur l'indentification de l'objet
+Cette information provient de l'objet virtuel créer sur le serveur Thingsbord
+*/
+
+#define SECRET_TOKEN "iblPR197nd9OIGESL8ex"
+#define SECRET_DEVICE_ID "6ca0e750-5ed6-11ed-8ee2-53dd4d2f7d52"
